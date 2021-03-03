2d0f2d16aac8010b2de7d8c28422bcf6326982a6
tangcs <tangcs@neusensetech.com>
2021-03-03 15:37:55 +0800

    test

---
+++ src/test/java/com/white/daily/utils/DateUtils.java
@@ -0,0 +1,17 @@
+package com.white.daily.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author tcs
+ * @date Created in 2021-03-03
+ */
+@SpringBootTest
+public class DateUtils {
+/*    DateUtils.
+    @Test
+    public static void test1(){
+        DateUtils
+    }*/
+}
