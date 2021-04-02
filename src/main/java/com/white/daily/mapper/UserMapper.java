09124b559d91ed33aff381f729e25389ff9cb2af
tangcs <tangcs@neusensetech.com>
2021-04-02 13:36:35 +0800

    测试sql性能 添加logback-spring.xml

--- src/main/java/com/white/daily/mapper/UserMapper.java
+++ src/main/java/com/white/daily/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.white.daily.mapper;
 
 import com.white.daily.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -18,4 +19,10 @@ public interface UserMapper {
      * @return result
      */
     List<User> selectUserList();
+
+    void addUserList(@Param("users") List<User> users);
+
+    void insert(@Param("user") User user);
+
+    List<User> selectUser(User user);
 }
