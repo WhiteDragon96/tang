1ca919159626f3fb5f8a949b92e2f0377007cc94
tangcs <tangcs@neusensetech.com>
2021-04-15 17:21:15 +0800

    Stream流Demo

--- src/main/java/com/white/daily/pojo/User.java
+++ src/main/java/com/white/daily/pojo/User.java
@@ -1,6 +1,8 @@
 package com.white.daily.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -9,6 +11,8 @@ import java.io.Serializable;
  * @date Created in 2021-04-02
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
